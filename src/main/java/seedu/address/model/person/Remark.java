9223b232a197163ddbf0f2e158a0ff6ba8a3fa03
chloelim <chloelim22@gmail.com>
2022-09-17 21:58:48 +0800

    Feat: Adding a new Command (Remark)
    Adds functionality such that each user now supports a remark field
    that can be added, deleted, and edited.

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,33 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+public class Remark {
+    public final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        } else if (!(other instanceof Remark)) {
+            return false;
+        }
+        Remark otherRemark = (Remark) other;
+        return otherRemark.value.equals(value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.commands;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Remark;
+import seedu.address.testutil.PersonBuilder;
+
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+class RemarkCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    void execute_addRemarkUnfilteredList_success() {
+        final String remarkString = "edited remark";
+        Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+
+        // expected result
+        Person expectedEditedPerson = new PersonBuilder(personToEdit).withRemark(remarkString).build();
+        String expectedSuccessMessage = String.format(RemarkCommand.MESSAGE_ADD_REMARK_SUCCESS, expectedEditedPerson);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs()); // copy model
+        expectedModel.setPerson(personToEdit, expectedEditedPerson);
+
+        // actual result
+        Remark remarkEdit = new Remark(remarkString);
+        RemarkCommand remarkCommand = new RemarkCommand(INDEX_FIRST_PERSON, remarkEdit);
+
+        // assert Success Message and Model are as expected
+        assertCommandSuccess(remarkCommand, model, expectedSuccessMessage, expectedModel);
+    }
+}
